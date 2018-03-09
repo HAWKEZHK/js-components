a67e78060b21f713212887f0cec1335198c4d327
HAWKEZHK <hawkezhk@outlook.com>
2018-03-09 22:56:25 +0800

    js

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 * 主要用于锻炼原生能力
 * 学习并整理一些常用的设计模式
 * 会写一些现在UI框架提供的组件，但不是提供使用，只为理解其中做法
+
+### 目前完成
++ **loopImgs**<br/>
+简介：面向对象轮播图<br/>
+在线预览: [传送门](http://htmlpreview.github.io/?https://github.com/HAWKEZHK/js-components/blob/master/LoopImgs/example/index.html)
+
++ **progress-indicator**<br/>
+简介：类似阮一峰老师的《ECMAScript 6 入门》的那个网站的顶部的进度条<br/>
+在线预览: [传送门](http://htmlpreview.github.io/?https://github.com/HAWKEZHK/js-components/blob/master/progress-indicator/example/index.html)
+
++ **mvvm**<br/>
+简介：模拟vue双向绑定原理<br/>
+在线预览: [传送门](http://htmlpreview.github.io/?https://github.com/HAWKEZHK/js-components/blob/master/mvvm/example/index.html)
--- progress-indicator/index.js
+++ progress-indicator/index.js
@@ -1,124 +1,127 @@
-;(function(window) {
-  //订阅者列表
-  let listener = {
-    end: [],
-    change: []
-  };
+(function(window) {
+	// 注册的回调函数
 
-  //工具函数
-  let utils = {
-    extend(a, b) {
-      for (let key in b) {
-        if (b.hasOwnProperty(key)) {
-          a[key] = b[key];
-        }
-      }
-      return a;
-    },
-    getMetrics(el) {
-      return {
-        scrollHeight: document.documentElement.scrollHeight || document.body.scrollHeight,
-        scrollTop: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop,
-        clientWidth: document.documentElement.clientWidth,
-        clientHeight: document.documentElement.clientHeight
-      };
-    },
-    onCallback(type) {
-      listener[type].forEach((cb, i) => {
-        cb();
-        if (cb.once) { listener[type].splice(i, 1); }
-      });
-    }
-  };
+	let listener = {
+		end: [],
+		change: []
+	}
 
-  //私有方法
-  let _ = {
-    //dom对象
-    $el: null,
-    //配置
-    $option: {
-      color: '#2b85e4'
-    },
-    //初始化函数
-    init(option) {
-      _.$option = utils.extend(_.$option, option);
-      _.create(arguments);
-      if (document.addEventListener) {
-        document.addEventListener('scroll', _.onScroll);
-      } else {
-        document.attachEvent('onscroll', _.onScroll);
-      }
-    },
-    // 创建dom
-    create() {
-      _.$el = document.createElement('div');
-      _.$el.style.height = '5px';
-      _.$el.style.width = '0px';
-      _.$el.style.position = 'fixed';
-      _.$el.style.top = '0';
-      _.$el.style.left = '0';
-      _.$el.style.backgroundColor = _.$option.color;
-      document.body.appendChild(_.$el);
-    },
-    //页面滚动的回调函数
-    onScroll() {
-      let metrics = utils.getMetrics();
-      let curPos = metrics.scrollHeight <= metrics.scrollTop + metrics.clientHeight
-        ? metrics.clientWidth 
-        : Math.floor(metrics.clientWidth * (metrics.scrollTop / (metrics.scrollHeight - metrics.clientHeight)));
-      _.$el.style.width = curPos + 'px';
+	// 工具函数
+	let utils = {
+		extend(a, b) {
+			for (let key in b) {
+				if (b.hasOwnProperty(key)) {
+					a[key] = b[key];
+				}
+			}
+			return a;
+		},
+		getMetrics(el) {
+			return {
+				scrollHeight: document.documentElement.scrollHeight || document.body.scrollHeight,
+				scrollTop: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop,
+				clientWidth: document.documentElement.clientWidth,
+				clientHeight: document.documentElement.clientHeight
+			};
+		},
+		onCallback(type) {
+			listener[type].forEach((cb, i) => {
+				cb();
+				if (cb.once) {
+					listener[type].splice(i, 1);
+				}
+			});
+		}
+	}
 
-      setTimeout(() => {
-        if (metrics.scrollHeight <= metrics.scrollTop + metrics.clientHeight) {
-          utils.onCallback('end');
-        }
-        utils.onCallback('change');
-      }, 50);
-    },
-  }
-  //构造函数
-  function progressIndicator(option) {
-    _.init(option)
-  }
+	// 私有的方法
+	let _ = {
+		// progress-indicator dom对象
+		$el: null,
+		// 配置
+		$option: {
+			color: '#2b85e4'
+		},
+		// 初始化函数
+		init(option) {
+			this.$option = utils.extend(this.$option, option);
+			this.create(arguments);
+			this.onScroll = this.onScroll.bind(this);
+			if (document.addEventListener) {
+				document.addEventListener('scroll', this.onScroll);
+			} else {
+				document.attachEvent('onscroll', this.onScroll);
+			}
+		},
+		// 创建dom
+		create() {
+			this.$el = document.createElement('div');
+			this.$el.style.height = '5px';
+			this.$el.style.width = '0px';
+			this.$el.style.position = 'fixed';
+			this.$el.style.top = '0';
+			this.$el.style.left = '0';
+			this.$el.style.backgroundColor = this.$option.color;
+			document.body.appendChild(this.$el);
+		},
+		// 页面滚动的回调函数
+		onScroll() {
+			let metrics = utils.getMetrics();
+			let curPos = metrics.scrollHeight <= metrics.scrollTop + metrics.clientHeight
+				? metrics.clientWidth
+				: Math.floor(metrics.clientWidth * (metrics.scrollTop / (metrics.scrollHeight - metrics.clientHeight)));
+			this.$el.style.width = curPos + 'px';
 
-  // 公有的方法
-  // 该类只有监听结束事件，和改变事件
-  // 分别对应 end 和 change
-  // 监听类型有三种, 一个只监听一次，一个一直监听， 一个取消监听
+			setTimeout(() => {
+				if (metrics.scrollHeight <= metrics.scrollTop + metrics.clientHeight) {
+					utils.onCallback('end');
+				}
+				utils.onCallback('change');
+			}, 50);
+		},
+	}
+	// 构造函数
+	function progressIndicator(option) {
+		_.init(option)
+	}
 
-  let prop = progressIndicator.prototype;
+	// 公有的方法
+	// 该类只有监听结束事件，和改变事件
+	// 分别对应 end 和 change
+	// 监听类型有三种, 一个只监听一次，一个一直监听， 一个取消监听
 
-  prop.on = function (type, fn) {
-    let cbs = listener[type];
-    let index = cbs.indexOf(fn);
-    if (index !== -1) {
-      cbs[index] = fn;
-    } else {
-      cbs.push(fn);
-    }
-  };
+	let prop = progressIndicator.prototype;
 
-  prop.once = function (type, fn) {
-    fn.once = true;
-    prop.on(type, fn);
-  };
+	prop.on = function (type, fn) {
+		let cbs = listener[type];
+		let index = cbs.indexOf(fn);
+		if (index !== -1) {
+			cbs[index] = fn;
+		} else {
+			cbs.push(fn);
+		}
+	}
 
-  prop.unsubscribe = function (type, fn) {
-    let cbs = listener[type];
-    listener[type] = cbs.filter(cb => cb !== fn);
-  };
+	prop.once = function (type, fn) {
+		fn.once = true;
+		prop.on(type, fn);
+	}
+	prop.unsubscribe = function (type, fn) {
+		let cbs = listener[type];
+		listener[type] = cbs.filter(cb => cb !== fn);
+	}
 
-  prop.destoryed = function () {
-    if (document.removeEventListener) {
-      document.removeEventListener('scroll', _.onScroll);
-    } else {
-      document.detachEvent('onscroll', _.onScroll);
-    }
-    document.body.removeChild(_.$el);
-  };
+	prop.destoryed = function () {
+		if (document.removeEventListener) {
+			document.removeEventListener('scroll', _.onScroll);
+		} else {
+			document.detachEvent('onscroll', _.onScroll);
+		}
+		document.body.removeChild(_.$el);
+	}
 
-  // 最后将该组件挂载在window对象
+	// 最后将该组件挂载在window对象
 
-  window.progressIndicator = progressIndicator;
+	window.progressIndicator = progressIndicator;
 
 })(window);
